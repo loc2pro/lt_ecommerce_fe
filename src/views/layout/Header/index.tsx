b0dfea3be3a93e83efcc19cd1a2c1c23f919a208
Chicken Dev <locdev2000@gmail.com>
2023-10-27 15:57:46 +0700

    test before deploy

--- src/views/layout/Header/index.tsx
+++ src/views/layout/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './style.module.scss';
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return <div className={styles.header}>Header test</div>;
 };
 
 export default Header;
