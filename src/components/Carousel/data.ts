07bddd6b2110b91a49e738350674a5fe3842c818
Chicken Dev <locdev2000@gmail.com>
2023-10-30 19:57:10 +0700

    feat: implement carousel automatic

---
+++ src/components/Carousel/data.ts
@@ -0,0 +1,18 @@
+export const DataCarousel = [
+  {
+    id: 1,
+    image: '/assets/image/banner1.png',
+  },
+  {
+    id: 2,
+    image: '/assets/image/banner2.jpg',
+  },
+  {
+    id: 3,
+    image: '/assets/image/banner3.jpg',
+  },
+  {
+    id: 4,
+    image: '/assets/image/banner4.jpg',
+  },
+];
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Carousel from '@components/Carousel';
 
 const search = () => {
   return (
     <div>
       search page
+      <Carousel />
     </div>
-  )
-}
+  );
+};
 
-export default search
+export default search;
