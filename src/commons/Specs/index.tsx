94dc050d241b4d617a46666c47d75890f607357c
Dang Nguyen <nguyendangnguyen1606@gmail.com>
2023-12-20 18:12:30 +0700

    feat/about_Product: update about_Product

---
+++ src/commons/Specs/index.tsx
@@ -0,0 +1,34 @@
+import styled from './style.module.scss';
+import { useState } from 'react';
+
+interface ChildComponentProps  {
+  description: string;
+  colors: string[];
+}
+  const index: React.FunctionComponent<ChildComponentProps> = ({ colors, description }) => {
+  const [activeColor, setActiveColor] = useState(0);
+  const addActiveColor = (index: number) => {
+    setActiveColor(index);
+  };
+  console.log(3)
+
+  return (
+    <div className={styled.product_detai_box}>
+      <span className={styled.product_detai_description}>{description}</span>
+      <ul className={styled.product_detai_color}>
+        {Array.isArray(colors) && colors.length > 0
+          ? colors.map((color, index) => (
+              <li
+                key={index}
+                className={activeColor === index ? styled.activeColor : ''}
+                onClick={() => addActiveColor?.(index)}
+              >
+                <span style={{ background: `${color}` }}></span>
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
+};
+export default index;
