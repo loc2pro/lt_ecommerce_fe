f0bc136925687f0f195a20e2a248b967aa1adcdd
Dang Nguyen <nguyendangnguyen1606@gmail.com>
2024-03-15 17:17:35 +0700

    feat/branch_allProduct: update css

--- src/components/BrandAllProduct/index.tsx
+++ src/components/BrandAllProduct/index.tsx
@@ -48,9 +48,9 @@ const index = () => {
   const handlePageChange = (newPage: number) => {
     console.log('New page', newPage);
     setFilters({
-      ... filters,
-      _page : newPage,
-    })
+      ...filters,
+      _page: newPage,
+    });
   };
   const ContainerProduct = classNames({
     [styled.sectionProduct]: true,
@@ -59,22 +59,22 @@ const index = () => {
   const onChangeLayout = (signal: string) => {
     signal === 'grid' ? setGridLayout(true) : setGridLayout(!gridLayout);
   };
-  const [activeColor, setActiveColor] = useState(0);
+  const [activeColor, setActiveColor] = useState(1);
   const addActiveColor = (index: number) => {
     setActiveColor(index);
   };
   return (
-    <div>
+    <div className={styled.main}>
       <div className={styled.banner}>
         <img src="\assets\image\bannerAsus.jpg" alt="" />
       </div>
       <div className={styled.pape_breadcrumb}>
         <Link href="#">
-          <b>Home</b>
+          <span>Home</span>
         </Link>
         <span> &gt; </span>
         <Link href="#">
-          <b>Laptops</b>
+          <span>Laptops</span>
         </Link>
         <span> &gt; </span>
         <Link href="#">Everyday Use Notebooks </Link>
@@ -85,147 +85,183 @@ const index = () => {
       </div>
       <section className={styled.wrapper}>
         <h2>MSI PS Series (20)</h2>
-        <div className={styled.left__content}>
-          <button>&lt; Back</button>
-          <div>
-            <h3>Filters</h3>
-            <button>Clear Filters</button>
-            <div>
-              <ul>
-                <li>
-                  <span>Category</span>
+        <div className={styled.content}>
+          <div className={styled.left__content}>
+            <button className={styled.left__content_backIcon}>&lt; Back</button>
+            <div className={styled.left__content_filters}>
+              <h3>Filters</h3>
+              <button>Clear Filters</button>
+              <div className={styled.left__content_filters_category}>
+                <ul>
+                  <li>
+                    <span>Category</span>
+                    <img src="/assets/icon/down.svg" alt="DOWN ICON" />
+                  </li>
+                  <li>
+                    <span>CUSTOM PCS </span>
+                    <span>15</span>
+                  </li>
+                  <li>
+                    <span>MSI ALL-IN-ONE</span>
+                    <span>45</span>
+                  </li>
+                  <li>
+                    <span>HP/COMPAQ PCS</span>
+                    <span>1</span>
+                  </li>
+                </ul>
+              </div>
+              <div className={styled.left__content_filters_price}>
+                <ul>
+                  <li>
+                    <span>Price</span>
+                    <img src="/assets/icon/down.svg" alt="DOWN ICON" />
+                  </li>
+                  <li>
+                    <span>$0.00 - $1,000.00</span>
+                    <span>15</span>
+                  </li>
+                  <li>
+                    <span>$1,000.00 - $2,000.00</span>
+                    <span>45</span>
+                  </li>
+                  <li>
+                    <span>$2,000.00 - $3,000.00</span>
+                    <span>1</span>
+                  </li>
+                  <li>
+                    <span>$3,000.00 - $4,000.00</span>
+                    <span>1</span>
+                  </li>
+                  <li>
+                    <span> $4,000.00 - $5,000.00</span>
+                    <span>1</span>
+                  </li>
+                  <li>
+                    <span>$5,000.00 - $6,000.00</span>
+                    <span>1</span>
+                  </li>
+                  <li>
+                    <span>$6,000.00 - $7,000.00 </span>
+                    <span>1</span>
+                  </li>
+                  <li>
+                    <span>$7,000.00 And Above</span>
+                    <span>1</span>
+                  </li>
+                </ul>
+              </div>
+              <div className={styled.left__content_filters_color}>
+                <ul>
+                  <li>
+                    <span>Color</span>
+                    <img src="/assets/icon/down.svg" alt="DOWN ICON" />
+                  </li>
+                  <div>
+                    <li>
+                      <span style={{ background: `black` }}></span>
+                    </li>
+                    <li>
+                      <span style={{ background: `blue` }}></span>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+              <div className={styled.left__content_filters_name}>
+                <ul>
+                  <li>
+                    <span>Filter Name</span>
+                    <img src="/assets/icon/down.svg" alt="DOWN ICON" />
+                  </li>
+                  <button>Apply Filters (2)</button>
+                </ul>
+              </div>
+            </div>
+            <div className={styled.left__content_brands}>
+              <h3>Brands</h3>
+              <button>All Brands</button>
+              <div className={styled.left__content_brands_imgs}>
+                <img src="\assets\image\image 33-1.jpg" alt="" />
+                <img src="\assets\image\image 33-2.jpg" alt="" />
+                <img src="\assets\image\image 33-3.jpg" alt="" />
+                <img src="\assets\image\image 33-4.jpg" alt="" />
+                <img src="\assets\image\image 33-5.jpg" alt="" />
+                <img src="\assets\image\image 33.jpg" alt="" />
+              </div>
+              <div className={styled.left__content_brands_compare_products}>
+                <h3>Compare Products</h3>
+                <span>You have no items to compare.</span>
+              </div>
+              <div className={styled.left__content_brands_my_list}>
+                <h3>My Wish List</h3>
+                <span>You have no items in your wish list.</span>
+              </div>
+              <img src="\assets\image\image 49.jpg" alt="" />
+            </div>
+          </div>
+          <div className={styled.right__content}>
+            <div className={styled.sort_bar}>
+              <span>Items 1-35 of 61</span>
+              <div className={styled.right_sort_bar}>
+                <div>
+                  <span>
+                    Sort By: <b>Position</b>
+                  </span>
                   <img src="/assets/icon/down.svg" alt="DOWN ICON" />
-                </li>
-                <li>
-                  <span>CUSTOM PCS </span>
-                  <span>15</span>
-                </li>
-                <li>
-                  <span>MSI ALL-IN-ONE</span>
-                  <span>45</span>
-                </li>
-                <li>
-                  <span>HP/COMPAQ PCS</span>
-                  <span>1</span>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <ul>
-                <li>
-                  <span>Price</span>
+                </div>
+                <div>
+                  <span>
+                    Show: <b> 35 per page</b>
+                  </span>
                   <img src="/assets/icon/down.svg" alt="DOWN ICON" />
-                </li>
-                <li>
-                  <span>$0.00 - $1,000.00</span>
-                  <span>15</span>
-                </li>
-                <li>
-                  <span>$1,000.00 - $2,000.00</span>
-                  <span>45</span>
-                </li>
-                <li>
-                  <span>$2,000.00 - $3,000.00</span>
-                  <span>1</span>
-                </li>
-                <li>
-                  <span>$3,000.00 - $4,000.00</span>
-                  <span>1</span>
-                </li>
-                <li>
-                  <span> $4,000.00 - $5,000.00</span>
-                  <span>1</span>
-                </li>
-                <li>
-                  <span>$5,000.00 - $6,000.00</span>
-                  <span>1</span>
-                </li>
-                <li>
-                  <span>$6,000.00 - $7,000.00 </span>
-                  <span>1</span>
-                </li>
-                <li>
-                  <span>$7,000.00 And Above</span>
-                  <span>1</span>
-                </li>
-              </ul>
+                </div>
+                <div className={styled.icon_change_layout}>
+                  <img src="/assets/icon/iconGirdLayout.svg" alt="" onClick={() => onChangeLayout('grid')} />
+                  <br />
+                  <img src="/assets/icon/iconColumnsLayout2.svg" alt="" onClick={() => onChangeLayout('column')} />
+                </div>
+              </div>
             </div>
-            <div>
-              <ul className={styled.product_detai_color}>
-                <li key={1} className={activeColor === 1 ? styled.activeColor : ''} onClick={() => addActiveColor?.(1)}>
-                  <span style={{ background: 'black' }}></span>
-                </li>
-                <li key={2} className={activeColor === 2 ? styled.activeColor : ''} onClick={() => addActiveColor?.(2)}>
-                  <span style={{ background: 'while' }}></span>
-                </li>
-              </ul>
+            <div className={styled.total_product}>
+              <div>
+                <b>
+                  CUSTOM PCS <span>(24)</span>
+                </b>
+                <img src="/assets/icon/cancel.svg" alt="ICON CANCEL" />
+              </div>
+              <div>
+                <b>
+                  HP/COMPAQ PCS <span>(24)</span>
+                </b>
+                <img src="/assets/icon/cancel.svg" alt="ICON CANCEL" />
+              </div>
+              <button>Clear All</button>
             </div>
-            <div>
-              <ul>
-                <li>
-                  <span>Filter Name</span>
-                  <img src="/assets/icon/down.svg" alt="DOWN ICON" />
-                </li>
-                <button>Apply Filters (2)</button>
-              </ul>
+            <div className={ContainerProduct}>
+              {mockupData.map((product) => {
+                return <ProductCard product={product} gridLayout={gridLayout} />;
+              })}
+            </div>
+            <Pagination pagination={pagination} onPageChange={handlePageChange} />
+            <div className={styled.description}>
+              <span>
+                MSI has unveiled the Prestige Series line of business-class and gaming notebooks. Tuned for color
+                accuracy, the Prestige Series also leverages True Color Technology, which allows users to adjust the
+                display profile to best fit their computing needs. There are six different screen profiles, which are
+                tuned for gaming, reducing eye fatigue, sRGB color accuracy, increasing clarity for words and lines,
+                reducing harmful blue light, and optimizing contrast for watching movies. Given the various display
+                profiles and discrete graphics chip, the Prestige Series notebooks can be used for various design work
+                as well as for office tasks given that the screen can be adjusted for better clarity, color accuracy, or
+                for eye strain reduction. Users working with video or 3D rendering will appreciate the "movie mode" for
+                which contrast is increased. Home users or students can benefit from the "anti-blue" and the "office
+                mode" options, both of which are designed to reduce eye strain. This is helpful when working on the
+                computer for extended periods of time. Additionally, in their down time, students can also use the
+                "gamer mode" to increase the screen brightness.
+              </span>
+              <div className={styled.blur_text}></div>
+              <button>More</button>
             </div>
           </div>
-          <div>
-            <h3>Brands</h3>
-            <button>All Brands</button>
-            <div>
-              <img src="\assets\image\image 33-1.jpg" alt="" />
-              <img src="\assets\image\image 33-2.jpg" alt="" />
-              <img src="\assets\image\image 33-3.jpg" alt="" />
-              <img src="\assets\image\image 33-4.jpg" alt="" />
-              <img src="\assets\image\image 33-5.jpg" alt="" />
-              <img src="\assets\image\image 33.jpg" alt="" />
-            </div>
-            <div>
-              <h3>Compare Products</h3>
-              <span>You have no items to compare.</span>
-            </div>
-            <div>
-              <h3>My Wish List</h3>
-              <span>You have no items in your wish list.</span>
-            </div>
-            <img src="\assets\image\image 49.jpg" alt="" />
-          </div>
-        </div>
-        <div className={styled.right__content}>
-          <div>
-            <input type="text" placeholder="Items 1-35 of 61" />
-            <div>
-              <span> Sort By: Position </span>
-              <img src="/assets/icon/down.svg" alt="DOWN ICON" />
-            </div>
-            <div>
-              <span>Show: 35 per page </span>
-              <img src="/assets/icon/down.svg" alt="DOWN ICON" />
-            </div>
-            <div>
-              <img src="/assets/icon/iconiconGirdLayout.svg" alt="" onClick={() => onChangeLayout('grid')} />
-              <br />
-              <img src="/assets/icon/iconColumnsLayout2.svg" alt="" onClick={() => onChangeLayout('column')} />
-            </div>
-          </div>
-          <div>
-            <div>
-              <span>CUSTOM PCS (24)</span>
-              <img src="/assets/icon/cancel.svg" alt="ICON CANCEL" />
-            </div>
-            <div>
-              <span>HP/COMPAQ PCS (24)</span>
-              <img src="/assets/icon/cancel.svg" alt="ICON CANCEL" />
-            </div>
-            <button>Clear All</button>
-          </div>
-          <div className={ContainerProduct}>
-            {mockupData.map((product) => {
-              return <ProductCard product={product} gridLayout={gridLayout} />;
-            })}
-          </div>
-          <Pagination pagination={pagination} onPageChange={handlePageChange} />
         </div>
       </section>
     </div>
